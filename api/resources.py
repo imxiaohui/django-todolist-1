a3fab3401ed58d51e930ae1d00b6ab0a73dd8a43
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-04-06 11:21:15 +0200

    Added tastypie style resources.

---
+++ api/resources.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+
+from tastypie.resources import ModelResource
+from tastypie.serializers import Serializer
+
+from lists.models import TodoList, Todo
+
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'users'
+        excludes = ['email', 'password', 'is_superuser']
+        allowed_methods = ['get']
+
+
+class TodoListResource(ModelResource):
+    class Meta:
+        queryset = TodoList.objects.all()
+        resource_name = 'todolists'
+        allowed_methods = ['get']
+
+
+class TodoResource(ModelResource):
+    class Meta:
+        queryset = Todo.objects.all()
+        resource_name = 'todos'
+        allowed_methods = ['get']
